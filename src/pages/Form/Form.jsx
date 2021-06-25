596cf1776b2a2356528a572b431efaa28574208f
arihant jain <jain.ari2000@gmail.com>
2021-06-25 20:51:10 +0530

    form section and increased padding to skills

--- src/pages/Form/Form.jsx
+++ src/pages/Form/Form.jsx
@@ -12,6 +12,13 @@ const Form = () => {
             <div className="form__left">
                 <div className="form__heading-3 heading-3">Let’s make something new, different and more meaningful or make thing  more visual or Conceptual ? Just Say Hello !</div>
                 <div className="form__dasharrow"><DashArrow/></div>
+                <div className="form__details">
+                    <div className="detail"><span className='u-color-primary-light'>—</span> +917239873575</div>
+                    <div className="detail"><span className='u-color-primary-light'>—</span> jain.ari2000@gmail.com</div>
+                    <div className="detail"><span className='u-color-primary-light'>—</span> IIIT Jabalpur, Near Dumna Airport, 482005</div>
+
+
+                </div>
             </div>
 
             <div className="form__formelement"><FormElement/></div>
