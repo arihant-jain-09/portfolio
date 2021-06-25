fbb471bd819380bddc08b85d6efbc02baa8a010f
arihant jain <jain.ari2000@gmail.com>
2021-06-25 19:08:24 +0530

    added dash arrow to form

--- src/pages/Form/Form.jsx
+++ src/pages/Form/Form.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Form.scss'
 import FormElement from '../../components/Form/FormElement'
+import {ReactComponent as DashArrow} from '../../assets/DashArrow.svg'
 const Form = () => {
     return (<>
         <div className="form__background">
@@ -8,8 +9,11 @@ const Form = () => {
         </div>
         
         <div className='form'>
-            <div className="form__heading-3 heading-3">Let’s make something new, different and more meaningful or make thing  more visual or Conceptual ? Just Say Hello !</div>
-            
+            <div className="form__left">
+                <div className="form__heading-3 heading-3">Let’s make something new, different and more meaningful or make thing  more visual or Conceptual ? Just Say Hello !</div>
+                <div className="form__dasharrow"><DashArrow/></div>
+            </div>
+
             <div className="form__formelement"><FormElement/></div>
         </div>
         </>
