45a5416e6ee32b4a1bc19c3b8f754bbecb639449
arihant jain <jain.ari2000@gmail.com>
2021-06-25 21:22:33 +0530

    changed text

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -14,7 +14,7 @@ const About = () => {
         </div>
         <div className='about'>
             <div className="about__section"><Section text="About">About Me</Section></div>
-            <div className="about__paragraph paragraph">I'm a <span className="u-color-primary-light">Professional Front-End developer</span> and I am very passionate and dedicated to my work, I have acquired the skills and knowledge necessary to make your project a success, I enjoy every step of the Web Development process, from building the base of the app to the final product, but I find the most satisfaction in seeing the finished product do everything for you that it was created to do.</div>
+            <div className="about__paragraph paragraph">I'm a <span className="u-color-primary-light">Professional Full-stack developer</span> and I am very passionate and dedicated to my work, I have acquired the skills and knowledge necessary to make your project a success, I enjoy every step of the Web Development process, from building the base of the app to the final product, but I find the most satisfaction in seeing the finished product do everything for you that it was created to do.</div>
             <div className="about__signature"><Signature/></div>
             <div className="about__button"><Button type="primary">Hire Me</Button></div>
         </div>
--- src/pages/HeroSection/HeroSection.jsx
+++ src/pages/HeroSection/HeroSection.jsx
@@ -25,7 +25,7 @@ const HeroSection = () => {
                 <div className="hero-section-line"><Line/></div>
                 <div className="heading-4 hero-section__heading-4 u-color-primary"><span>HELLO</span></div>
                 <div className="heading-1 hero-section__heading-1">I'm <span className='u-color-primary'>Arihant</span> Jain</div>
-                <div className="paragraph hero-section__paragraph">This is <span className='u-color-primary-light'>Arihant Jain,</span> Front-end Developer, interests in Web Frameworks and technologies, looking for work</div>
+                <div className="paragraph hero-section__paragraph">This is <span className='u-color-primary-light'>Arihant Jain,</span> Full Stack Developer, interests in Web Frameworks and technologies, looking for work</div>
                 <div className="hero-section__button"><Button type='primary'>Download CV</Button></div>
             <div className="hero-section__image">
 
