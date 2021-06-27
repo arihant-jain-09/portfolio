906049ba1a6db5e2ca35dcaf32ef06e0f504f5dc
arihant jain <jain.ari2000@gmail.com>
2021-06-27 10:43:39 +0530

    added image to about section

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -5,6 +5,7 @@ import '../../styles/Elements/elements.scss'
 import {ReactComponent as DottedBig} from '../../assets/dotted-big.svg'
 import {ReactComponent as DottedSmall} from '../../assets/dotted-small.svg'
 import {ReactComponent as Signature} from '../../assets/signature.svg'
+import {ReactComponent as Line} from '../../assets/line.svg'
 import Button from '../../components/Button/Button'
 const About = () => {
     return (<>
@@ -14,9 +15,16 @@ const About = () => {
         </div>
         <div className='about'>
             <div className="about__section"><Section text="About">About Me</Section></div>
-            <div className="about__paragraph paragraph">I'm a <span className="u-color-primary-light">Professional Full-stack developer</span> and I am very passionate and dedicated to my work, I have acquired the skills and knowledge necessary to make your project a success, I enjoy every step of the Web Development process, from building the base of the app to the final product, but I find the most satisfaction in seeing the finished product do everything for you that it was created to do.</div>
-            <div className="about__signature"><Signature/></div>
-            <div className="about__button"><Button type="primary">Hire Me</Button></div>
+            <div className="about__left">
+                <img src="./ab.jpg" alt="About" />
+            </div>
+            <div className="about__right">
+                <div className="about-line"><Line/></div>
+                <div className="heading-4 about__heading-4 u-color-primary"><span>LET ME INTRODUCE MYSELF</span></div>
+                <div className="about__paragraph paragraph--big">I'm a <span className="u-color-primary-light">Professional Full-stack developer</span> and I am very passionate and dedicated to my work, I have acquired the skills and knowledge necessary to make your project a success, I enjoy every step of the Web Development process, from building the base of the app to the final product, but I find the most satisfaction in seeing the finished product do everything for you that it was created to do.</div>
+                <div className="about__signature"><Signature/></div>
+                <div className="about__button"><Button type="primary">Hire Me</Button></div>
+            </div>
         </div>
         </>
     )
